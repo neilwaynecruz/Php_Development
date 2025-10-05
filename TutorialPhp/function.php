37b34cf6291b600d499e7b5ec62dc13c19e95352
Neil Wayne <neilwayne0514@gmail.com>
2025-10-06 01:20:43 +0800

    Added files via 'git add'.

--- TutorialPhp/function.php
+++ TutorialPhp/function.php
@@ -11,8 +11,9 @@
     // 1. User-defined functions
     // 2. Built-in functions
 
+        echo "<H1>User-defined Functions</H1>";
         // 1. USER-DEFINED FUNCTIONS
-        function addNumbers($a, $b){
+        function addNumbers($a, $b):mixed{
             $sum = $a + $b;
             return $sum;
         }
@@ -20,24 +21,46 @@
         $result = addNumbers(10, 20);
         echo "The sum is: {$result} <br>";
 
-        echo "<br>";
-
-        function multiplyNumbers($x, $y){   
+        function multiplyNumbers($x, $y):mixed {   
             $product = $x * $y;
             return $product;
         }
-
         $result2 = multiplyNumbers(5, 4);
         echo "The product is: {$result2} <br>";
+
+        // with default parameters
+        function taxCalculation($amount, $taxRate = 0.05):mixed {
+            $tax = $amount * $taxRate;
+            return $tax;
+        }
+        $result3 = taxCalculation(1000);
+
+        // reference parameters
+        function increment(&$value):void {
+            $value++;
+        }
+        $num = 5;
+        increment($num);
+        echo "The incremented value is: {$num} <br>";
+
+        // variable length arguments
+        function sumMyNumbers(...$numbers):mixed {
+            $total = 0;
+            foreach ($numbers as $number) {
+                $total += $number;
+            }
+            return $total;
+        }
+        $sum = sumMyNumbers(1, 2, 3, 4, 5);
+        echo "The total sum is: {$sum} <br>";
 
         echo "<br>";
 
         // 2. BUILT-IN FUNCTIONS
+        echo "<H1>Built-in Functions</H1>";
         $str = "Hello, welcome to PHP functions!";
         $length = strlen($str);
         echo "The length of the string is: {$length} <br>";
-
-        echo "<br>";
 
         $number = 7.8;
         $rounded = round($number);
