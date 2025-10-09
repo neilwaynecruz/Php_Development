379b10a482d7b14b944314b246bd23e8c4d5b1fa
Neil Wayne <neilwayne0514@gmail.com>
2025-10-10 02:56:47 +0800

    php 12th commit

--- lesson3/assignment/arrays.php
+++ lesson3/assignment/arrays.php
@@ -27,6 +27,11 @@
             border-bottom: 2px solid #d4af37;
             display: inline-block;
             margin-bottom: 10px;
+            transition: all 0.3s ease-in-out;
+        }
+
+        h3:hover{
+            transform: scale(1.2);
         }
 
         .container {
@@ -47,29 +52,32 @@
 
         li {
             margin-bottom: 3px;
+            transition: all 0.15s ease-in-out;
+        }
+
+        li:hover{
+            color: #bf0404ff;
+            transform: scale3d(1.03, 1.03, 1);
         }
 
         .section {
             margin-bottom: 30px;
         }
-
         .sched-box {
             background-color: #fff9e6;
             border: 1px solid #d4af37;
             padding: 12px 18px;
             border-radius: 6px;
             margin-bottom: 12px;
+            line-height: 1.8;
             transition: all 0.2s ease-in-out;
         }
-
         .sched-box:hover{
-            transform: scale(1.03);             /* gently enlarges the box */
+            transform: scale(1.03);            
             background-color: #ffffffff;
             color: rgba(7, 66, 192, 1);
             box-shadow: 0 3px 3px 1px rgba(0, 0, 0, 0.1);
         }
-
-
         .sched-box b {
             color: #800000;
         }
