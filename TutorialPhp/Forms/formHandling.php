875f6bf7b9407e06d63f4780ca5f5e9c6d58a18b
Neil Wayne <neilwayne0514@gmail.com>
2025-10-17 17:09:30 +0800

    Added

--- TutorialPhp/Forms/formHandling.php
+++ TutorialPhp/Forms/formHandling.php
@@ -1,1 +1,23 @@
-1
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Handling</title>
+</head>
+<body>
+    
+</body>
+
+<?php
+    function varArr(... $arr) {
+        foreach( $arr as $k) {
+            echo $k + 1,"", $k,"";
+        }
+        echo count($arr);
+    }
+    
+    varArr(1,2,3,4,5);
+?>
+</html>
+
