f8ee92a7bd70e1cf6f27d8ef5aaf2fb71d832c02
Neil Wayne <neilwayne0514@gmail.com>
2025-10-31 20:34:48 +0800

    php 23rd commit

---
+++ TutorialPhp/basics/include/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <hr>
+    Creator: Neil Wayne Cruz
+    <div><a href="neilwaynefake@gmail.com">neil0514@fake.com</a></div>
+</footer>
---
+++ TutorialPhp/basics/include/index.php
@@ -0,0 +1,16 @@
+<?php include("header.php");?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <div>This is the Home page</div>
+    <div>Stuff about your Home page can</div>
+    <div>go here</div>
+</body>
+</html>
+<?php include("footer.php");?>
