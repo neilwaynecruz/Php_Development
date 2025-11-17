99203e7c5b9edb90bdeb7a635633207ed9679e8c
Neil Wayne <neilwayne0514@gmail.com>
2025-11-17 21:32:31 +0800

    Laravel Added

---
+++ laravel_tutorial_1st/resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Test</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="text-center text-primary">Hello, Bootstrap!</h1>
+        <div class="text-center mt-4">
+            <button class="btn btn-success">Click Me</button>
+        </div>
+    </div>
+</body>
+</html>
